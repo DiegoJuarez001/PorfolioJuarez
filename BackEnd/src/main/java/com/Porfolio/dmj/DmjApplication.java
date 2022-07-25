7ee380e3adf91c1a51530efca4bac6bb1071a1f0
DiegoJuarez001 <juarezdiego1991@gmail.com>
2022-07-25 20:47:23 -0300

    porfoliodiegojuarez

---
+++ BackEnd/src/main/java/com/Porfolio/dmj/DmjApplication.java
@@ -0,0 +1,13 @@
+package com.Porfolio.dmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmjApplication.class, args);
+	}
+
+}
